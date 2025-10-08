4072c9e9d6f3d7fa6e9bcb7121e38f7f472272c5
Carlos David Marquez Valencia <carlos.marquez@inmel.co>
2025-10-08 15:51:24 -0500

    se actualiza los datos.

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Primer servidor")
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+// ruta de unicio
+app.get('/', (req, res)=> {
+    res.send("Servidor desde Express!");
+});
+
+// aqui vamos a iniciar el servidor
+app.listen(port, () =>{
+    console.log(`Sevidor escuchando en http://localhost:${port}`)
+});
+
+
+
+
